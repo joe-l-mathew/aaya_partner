8129fc47bd53d399926caecad30057bfa813a4d1
joe-l-mathew <84284015+joe-l-mathew@users.noreply.github.com>
2023-10-28 23:40:08 +0530

    Route till Worker home done

--- lib/screens/onboarding/verification_pending_screen.dart
+++ lib/screens/onboarding/verification_pending_screen.dart
@@ -1,10 +1,28 @@
-import 'package:flutter/widgets.dart';
+import 'package:aaya_partner/controller/worker_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class VerificationPendingScreen extends StatelessWidget {
   const VerificationPendingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    WorkerController workerController = Get.find();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Text(
+                "Aadhar: ${workerController.userData.value!.workerVerificationStatus.isAadharVerified}"),
+            Text(
+                "Pancard: ${workerController.userData.value!.workerVerificationStatus.isPanVerified}"),
+            Text(
+                "Image: ${workerController.userData.value!.workerVerificationStatus.isImageVerified}"),
+            Text(
+                "Complete verification: ${workerController.userData.value!.workerVerificationStatus.isVerified}"),
+          ],
+        ),
+      ),
+    );
   }
 }
